24c3a3ea8fc0b266f1d0183e2b0ea93572b8c6d0
Chesley Brown <chezbrown@gmail.com>
2014-01-07 18:55:58 -0330

    v1 of the autofill directive

---
+++ autofill-directive.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(ngModule) {
+	var force
+	ngModule.directive('autofill', function() {
+		return {
+			require: 'ngModel',
+			link: function (scope, element, attrs, ngModel) {
+				scope.$on('autofill-submit', function(e) {
+					ngModel.$setViewValue(element.val());
+				});
+			}
+		}
+	});
+	
+	ngModule.directive('autofillSubmit', ['$parse', function($parse) {
+		return {
+			link: function (scope, element) {
+				var fn = $parse(element.attr('autofill-submit'));
+				element.on('submit', function(event) {
+					scope.$broadcast('autofill-submit');
+					scope.$apply(function() {
+						fn(scope, {$event:event});
+					});
+				});
+			}
+		}
+	}]);
+};
