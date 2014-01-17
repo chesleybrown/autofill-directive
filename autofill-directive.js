5766aa34e3c88028dac85d7b2f18441476da79cd
Chesley Brown <chezbrown@gmail.com>
2014-01-17 09:35:30 -0330

    Formatting fixes and setting to v2

--- autofill-directive.js
+++ autofill-directive.js
@@ -1,28 +1,27 @@
 'use strict';
 
 angular.module('autofill-directive', [])
-
-		.directive('autofill', function() {
-				return {
-					require: 'ngModel',
-					link: function (scope, element, attrs, ngModel) {
-						scope.$on('autofill-submit', function(e) {
-							ngModel.$setViewValue(element.val());
-						});
-					}
-				}
-			})
-
-		.directive('autofillSubmit', ['$parse', function($parse) {
-			return {
-				link: function (scope, element) {
-					var fn = $parse(element.attr('autofill-submit'));
-					element.on('submit', function(event) {
-						scope.$broadcast('autofill-submit');
-						scope.$apply(function() {
-							fn(scope, {$event:event});
-						});
-					});
-				}
+	.directive('autofill', function() {
+		return {
+			require: 'ngModel',
+			link: function (scope, element, attrs, ngModel) {
+				scope.$on('autofill-submit', function(e) {
+					ngModel.$setViewValue(element.val());
+				});
 			}
-		}]);
+		}
+	})
+	
+	.directive('autofillSubmit', ['$parse', function($parse) {
+		return {
+			link: function (scope, element) {
+				var fn = $parse(element.attr('autofill-submit'));
+				element.on('submit', function(event) {
+					scope.$broadcast('autofill-submit');
+					scope.$apply(function() {
+						fn(scope, {$event:event});
+					});
+				});
+			}
+		}
+	}]);
